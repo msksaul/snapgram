fc038d02c6ffeb483fe25e1a35b08a108c3064ad
mscalel <msksaul@gmail.com>
2023-11-09 17:04:09 -0600

    Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import './globals.css'
 import SigninForm from './_auth/forms/SigninForm'
 import SignupForm from './_auth/forms/SignupForm'
-import { Home } from './_root/pages'
+import { AllUsers, CreatePost, EditPost, Explore, Home, LikedPosts, PostDetails, Profile, Saved, UpdateProfile } from './_root/pages'
 import AuthLayout from './_auth/AuthLayout'
 import RootLayout from './_root/RootLayout'
 import { Toaster } from "@/components/ui/toaster"
@@ -19,6 +19,15 @@ const App = () => {
 
         <Route element={<RootLayout />}>
           <Route index element={<Home />}/>
+          <Route path='/explore' element={<Explore />}/>
+          <Route path='/saved' element={<Saved />}/>
+          <Route path='/all-users' element={<AllUsers />}/>
+          <Route path='/create-post' element={<CreatePost />}/>
+          <Route path='/update-post/:id' element={<EditPost />}/>
+          <Route path='/posts/:id' element={<PostDetails />}/>
+          <Route path='/profile/:id/*' element={<Profile />}/>
+          <Route path='/update-profile/:id' element={<UpdateProfile />}/>
+          <Route path='/liked-posts' element={<LikedPosts />}/>
         </Route>
       </Routes>
 
