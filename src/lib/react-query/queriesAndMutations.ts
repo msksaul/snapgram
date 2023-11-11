6930eb19ec768bbda4ed7f117339a0c79b520c54
mscalel <msksaul@gmail.com>
2023-11-10 19:25:32 -0600

    Create Post

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -4,8 +4,9 @@ import {
   useQueryClient,
   useInfiniteQuery
 } from '@tanstack/react-query'
-import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
-import { INewUser } from '@/types'
+import { createUserAccount, signInAccount, signOutAccount, createPost, getRecentPosts } from '../appwrite/api'
+import { INewPost, INewUser } from '@/types'
+import { QUERY_KEYS } from './queryKeys'
 
 export const useCreateUserAccount = () => {
   return useMutation({
@@ -22,5 +23,24 @@ export const useSignInAccount = () => {
 export const useSignOutAccount = () => {
   return useMutation({
     mutationFn: signOutAccount
+  })
+}
+
+export const useCreatePost = () => {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: (post: INewPost) => createPost(post),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
+      })
+    }
+  })
+}
+
+export const useGetRecenPosts = () => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+    queryFn: getRecentPosts
   })
 }
